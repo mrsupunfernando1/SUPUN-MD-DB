4ae974f600611df0031d88e8cd3baba3b07c4898
Mr.supun fernando <166195531+mrsupunfernando1@users.noreply.github.com>
2025-04-19 04:50:45 +0530

    Create Data.js

---
+++ supun-md/Data.js
@@ -0,0 +1,11 @@
+{
+"footer": "> ꜰᴏʀᴡᴀʀᴅ ʙʏ ꜱᴜᴘᴜɴ ᴍᴅ",
+"mainimg": "https://i.ibb.co/mVJg1Ljn/Supunmd.jpg",
+"settingImg": "https://i.ibb.co/5W28Kbn0/4461.jpg",
+"version": "Supun Fernando",
+"owner": "3.4.0",
+"replyMsg":{
+  "emsg": "*ERORR..!*"
+
+}
+  }
